11723df63b343b923d328cc9b73208dde19fb8ef
Rosa Rodriguez Macbook <rosam1010.rr@gmail.com>
2023-01-02 21:46:56 +0100

    Cambios en columna

--- src/components/Columna.vue
+++ src/components/Columna.vue
@@ -24,6 +24,7 @@ defineProps({
       <Tarjeta titulo="Tarea 3" />
       <Tarjeta titulo="Tarea 3" />
       <Tarjeta titulo="Tarea 3" />
+      <TarjetaVacia> </TarjetaVacia>
     </div>
 
 </div>

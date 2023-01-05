09f89df0ef919a1e3bb534f7ce805a55a3a85599
Ilya Slyusarchuk <ilyaniesvb@gmail.com>
2023-01-05 18:04:45 +0100

    Drag & Drop

--- src/components/Columna.vue
+++ src/components/Columna.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="columna">
-    <h4 class="titulo-columna">{{ titulo }}</h4>
-    <div class="tarjetas">
-      <Tarjeta v-for="tarjeta in tarjetas" :key="tarjeta.id" :titulo="tarjeta.nombre"/>
+  <div class="columna" :id="'columna-' + id" @dragenter="dragEnter" @dragover="dragOver" @dragleave="dragLeave" @drop="drop">
+    <h4 class="titulo-columna" :id="'titulo-columna-' + id" style="pointer-events: none;">{{ titulo }}</h4>
+    <div class="tarjetas" :id="'tarjetas-columna-' + id">
+      <Tarjeta v-for="tarjeta in tarjetas" :key="tarjeta.id" :titulo="tarjeta.nombre" :id="tarjeta.id" :columnaId="id"/>
       <TarjetaVacia/>
     </div>
 </div>
@@ -33,13 +33,18 @@
     height: max-content;
 
     margin-left: 4px;
-    margin-right: 4px;
+    margin-right: 20px;
     border-radius: 0.375rem;
 }
 
 
 .tarjetas{
   overflow-x:hidden;
+}
+
+.drag-over {
+  background-color: #ccc;
+  border: 1px #172b4d dotted;
 }
 
 </style>
@@ -65,6 +70,60 @@ const props = defineProps({
     required: true
   }
 })
+
+const parentColumna = ref(null)
+
+const contadorElementos = ref(0)
+
+const dragEnter = (e) => {
+  contadorElementos.value += 1
+  e.preventDefault()
+  // Check if the element is a column and it starts with 'columna-'
+  if (e.target.id.startsWith('columna-')) {
+    console.log("Entrando a " + e.target.id)
+    e.target.classList.add('drag-over')
+    parentColumna.value = e.target
+  }
+}
+
+const dragOver = (e) => {
+  e.preventDefault()
+  if (e.target.id.startsWith('columna-')) {
+    e.target.classList.add('drag-over')
+    parentColumna.value = e.target
+  }
+}
+
+const dragLeave = (e) => {
+  setTimeout(() => {
+    contadorElementos.value -= 1
+    if (contadorElementos.value === 0 && e.target.id.startsWith('columna-')) {
+      parentColumna.value = null
+      e.target.classList.remove('drag-over')
+    }
+  }, 0)
+}
+
+const drop = (e) => {
+  contadorElementos.value = 0
+  parentColumna.value.classList.remove('drag-over')
+  let id = e.dataTransfer.getData('text/plain')
+  const draggedElement = document.getElementById(id)
+  const lastElement = parentColumna.value.children[1].children[parentColumna.value.children[1].children.length - 1]
+  parentColumna.value.children[1].insertBefore(draggedElement, lastElement)
+  draggedElement.classList.remove('hide')
+  const columna_id = id.split('-')[2]
+  const tarjeta_id = id.split('-')[3]
+  axios.patch('http://localhost:3000/tableros/' + props.tableroId + '/columnas/' + columna_id + '/tarjetas/' + tarjeta_id, {
+    columna: props.id
+  })
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
 const tarjetas = ref([])
 
--- src/components/Tarjeta.vue
+++ src/components/Tarjeta.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="cuadro-tarjeta">
+  <div class="cuadro-tarjeta" :id="'cuadro-tarjeta-' + columnaId + '-' + id" draggable="true" @dragstart="dragStart" @dragend="dragEnd">
     <h1>{{ titulo }}</h1>
   </div>
 </template>
 
 <script setup>
+import { onMounted } from "@vue/runtime-core"
 
-defineProps({
-  titulo: {
-    type: String,
-    required: true
+
+  defineProps({
+    titulo: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    columnaId: {
+      type: Number,
+      required: true
+    }
+  })
+
+  const dragStart = (e) => {
+    e.dataTransfer.setData('text/plain', e.target.id)
+
+    setTimeout(() => {
+      e.target.classList.add('hide')
+    }, 0)
+
+    // const id = e.dataTransfer.getData('text/plain')
+    // console.log(id)
   }
-})
+
+  const dragEnd = (e) => {
+    e.target.classList.remove('hide')
+  }
 </script>
 
 <style scoped>
@@ -23,6 +48,11 @@ defineProps({
     min-height: 32px;
     margin-bottom:8px;
     border-radius: 0.375rem;
+    cursor: pointer;
+}
+
+.hide {
+  display: none;
 }
 
 

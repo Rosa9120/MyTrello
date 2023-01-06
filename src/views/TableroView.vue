e49ec5f87beae7bb4de5b59ed51c0ae9f5eb7cb3
Rosa Rodriguez Macbook <rosam1010.rr@gmail.com>
2023-01-06 02:07:48 +0100

    Tablero ocupa toda anchura y se puede scrollear

--- src/views/TableroView.vue
+++ src/views/TableroView.vue
@@ -47,7 +47,7 @@ onMounted(async () => {
 
 <template>
 
-<div>
+<div class="layout">
   <h3 class="titulo"> Tablero {{ nombre }} </h3>
   <div class="cuadro">
       <Columna v-for="columna in columnas" :key="columna.id" :titulo="columna.titulo" :id="columna.id" :tableroId="props.id" />
@@ -61,29 +61,47 @@ onMounted(async () => {
 
 html,body{
     height:100%;
-    width: 100%;
+    width: 100vw;
+    max-width: 100vw;
     margin:0;
     padding:0;
+    overflow-x:clip;
 }
 
 #app{
     height:100%;
-    width: 100%;
+    width: 100vw;
+    max-width: 100vw;
+    margin:0;
+    padding:0;
     display:flex;
     flex-flow: column;
     background-color: whitesmoke;
+
 }
 
 .cuadro{
     background-color: whitesmoke;
     display:flex;
     flex-flow: row;
-    flex-grow: 1;
-    width: 100%;
+
+
 }
 
 .cuadro h1{
   text-align: center;
+}
+
+.layout{
+    margin-top:15px;
+    display:flex;
+    flex-flow: column wrap;
+    gap: 20px;
+    align-items:center;
+    max-width: 100%;
+    height:100%;
+    width: 100% !important;
+    overflow-x:scroll;
 }
 
 </style>

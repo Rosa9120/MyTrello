a92b507356786f7c4bef736ee44a44423f70332c
Rosa Rodriguez Macbook <rosam1010.rr@gmail.com>
2023-01-07 01:36:09 +0100

    Front editar tablero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,12 +43,38 @@ onMounted(async () => {
                   o quiza meterle descripcion al tablero
                   yo en la foto pondría la foto de background que tiene cada tablero. y si no tiene, pues un color aleatorio
                 </b-card-text>
+                    <div class="botones">
+                      <router-link :to="{ name: 'tablero', params: { id: tablero.id }}" custom v-slot="{ navigate }">
+                      <b-button @click="navigate" role="link" variant="primary"> Ver tablero </b-button>
+                    </router-link>
+                    <b-button data-bs-toggle="modal" :data-bs-target="'#exampleModal-' + tablero.id" variant="warning"> Gestionar </b-button>
+                </div>
 
-                <router-link :to="{ name: 'tablero', params: { id: tablero.id }}" custom v-slot="{ navigate }">
-                  <b-button @click="navigate" role="link" variant="primary"> Ver tablero </b-button>
-                </router-link>
               </b-card>
             </div>
+
+            <div class="modal fade" :id="'exampleModal-' + tablero.id" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"> Gestión de un tablero </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+
+                  <div class="modal-body">
+                    <b> Nombre: </b> <input type="text" required class="form-control" :value=tablero.nombre > <br>
+                    <b> Descripcion: </b> <b-form-textarea class="form-control" > </b-form-textarea> <br>
+                    <b> Imagen de fondo: </b> <input type="file" accept='image/*' class="form-control" > <br>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Borrar tablero</button>
+                    <button type="button" class="btn btn-primary">Guardar los cambios</button>
+                  </div>
+                </div>
+          </div>
+          </div>
           </span>
       </b-card-group>
 
@@ -103,6 +129,13 @@ html,body{
   display:flex;
   flex-flow: row nowrap;
   width: 90%;
+}
+
+.botones{
+  display:flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  width: 100%;
 }
 
 </style>
